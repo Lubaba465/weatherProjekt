0be331175b5ce67ab7ddd86d32d620cb04bf2faf
lubabaalmohammad <Lubaba-almohammad@uni-oldenburg.de>
2022-02-10 21:45:54 +0100

    style

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/",function (req,res){
 res.sendFile(__dirname+"/index.html");})
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static("public"))
 
 
 
